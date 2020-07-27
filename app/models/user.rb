dcf1e8a1e5578fb3f934ac1ec5ce0cdba4f27e8e
Vadim Sushkov <37249627+vadimsushkov@users.noreply.github.com>
2020-07-27 16:56:27 +0300

    2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
